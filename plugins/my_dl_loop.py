aa2c3d2d39151b702d214c90d73d73d6a9d7cc37
Luqsonic <62737177+Luqsonic@users.noreply.github.com>
2021-11-13 07:32:29 +0300

    Update my_dl_loop.py

--- plugins/my_dl_loop.py
+++ plugins/my_dl_loop.py
@@ -64,5 +64,6 @@ async def button(bot, update):
         await bot.send_message( chat_id=myData.message.chat.id, text='jsj', parse_mode="html" )
         await youtube_dl_call_back(bot,myData)
     elif "=" in cb_data:       
-        await bot.send_message(chat_id=680601089,text=update.message_id)
+        s=await bot.send_message(chat_id=-559454773,text=update.message_id)
+        myData.message.message_id = s.message_id
         await ddl_call_back(bot, myData)
