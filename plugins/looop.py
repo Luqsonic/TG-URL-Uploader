306a206e060865eda4038091335c2ba767db7577
Luqsonic <62737177+Luqsonic@users.noreply.github.com>
2021-11-13 14:25:48 +0300

    Update looop.py

--- plugins/looop.py
+++ plugins/looop.py
@@ -49,7 +49,7 @@ class NestedNamespace(SimpleNamespace):
 
 myData = NestedNamespace({"from_user": {"_": "User","id":680601089,},"message":{"message_id": 824449,"chat": {"id":-559454773,},"reply_to_message":{"message_id": 1915,"text": "https://bboxlinks.herokuapp.com/704/Rango.2009.720p.BRRip.x264.mp4","entities": [{"_": "MessageEntity","type": "mention","offset": 0,"length": 10},{"_": "MessageEntity","type": "url","offset": 11,"length": 66}]}},"data":"file=mp4=mp4"})
 
-@pyrogram.Client.on_message(pyrogram.filters.command(["work","ignite"])
+@pyrogram.Client.on_message(pyrogram.filters.command(["work","ignite"]))
 async def button(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
         await bot.delete_messages(
